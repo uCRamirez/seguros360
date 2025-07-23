d83f55018caec66424e9f318c1ea6c955623d4a0
uCRamirez <ucramirez@ucbusinesscr.com>
2025-07-23 17:42:31 -0600

    Ultima verision - cambios aplicados para update

--- app/Http/Requests/Api/Employee/StoreRequest.php
+++ app/Http/Requests/Api/Employee/StoreRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Api\User;
+namespace App\Http\Requests\Api\Employee;
 
 use Illuminate\Validation\Rule;
 use App\Http\Requests\Api\BaseRequest;
--- app/Http/Requests/Api/FormUCB/UpdateRequest.php
+++ app/Http/Requests/Api/FormUCB/UpdateRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Api\Form;
+namespace App\Http\Requests\Api\FormUCB;
 
 use App\Http\Requests\Api\BaseRequest;
 
